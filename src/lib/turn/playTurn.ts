e8b4615d0ad44314e9def4c7ed219d3f4bf269eb
Riobis <riobisofc@gmail.com>
2026-01-17 16:57:33 -0300

    fix lib

--- src/lib/turn/playTurn.ts
+++
@@ -1,8 +0,0 @@
-import type { Character } from "../../types/Character"
-import selectWeapon from "../../ui/combat/selectWeapon"
-import selectTarget from "../play/selectTarget"
-
-export default async function playTurn(character: Character) {
-  const target = await selectTarget(character)
-  await selectWeapon(character, target)
-}
