01b871265ad040ccb82a5a455789757a0254c11d
Riobis <riobisofc@gmail.com>
2026-02-08 14:50:51 -0300

    implemented classesPage

--- src/app/rpg/[rpgId]/classes/page.tsx
+++ src/app/rpg/[rpgId]/classes/page.tsx
@@ -1,17 +1,57 @@
+"use client" 
+import { useState } from "react"
 import Link from "next/link"
 import styles from "./page.module.css"
 import { classes } from "@/data/rpg/world-of-clans/classes"
 import rpgItem from "@/data/rpg/world-of-clans/rpg"
 
 export default function ClassPage() {
+  const [openCategory, setOpenCategory] = useState<string | null>(null)
+
+  const groupedClasses = classes.reduce(
+    (acc, item) => {
+      const cat = item.category || "Outros"
+      if (!acc[cat]) acc[cat] = []
+      acc[cat].push(item)
+      return acc
+    },
+    {} as Record<string, typeof classes>,
+  )
+  function capitalize(str: string) {
+    if (!str) return ""
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  const toggleCategory = (category: string) => {
+    setOpenCategory(openCategory === category ? null : category)
+  }
+
   return (
     <div className={styles.container}>
-      {classes.map((classe) => (
-        <section key={classe.id} className={styles.card}>
-          <Link href={`/rpg/${rpgItem.id}/classes/${classe.id}`}>
-            <h2 className={styles.title}>{classe.name}</h2>
-          </Link>
-        </section>
+      {Object.entries(groupedClasses).map(([category, items]) => (
+        <div key={category} className={styles.categoryWrapper}>
+          <button
+            className={styles.toggleButton}
+            onClick={() => toggleCategory(category)}
+          >
+            {capitalize(category)}
+            <span>{openCategory === category ? "▲" : "▼"}</span>
+          </button>
+
+          {openCategory === category && (
+            <div className={styles.grid}>
+              {items.map((classe) => (
+                <Link
+                  href={`/rpg/${rpgItem.id}/classes/${classe.id}`}
+                  key={classe.id}
+                  className={styles.card}
+                >
+                  <h3 className={styles.title}>{classe.name}</h3>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       ))}
     </div>
   )
--- src/data/rpg/world-of-clans/classes.ts
+++ src/data/rpg/world-of-clans/classes.ts
@@ -3,60 +3,135 @@ import { ninjaAbilities } from "./abilities/ninjaAbilities"
 
 export const classes = [
   // Arqueiros
-  { abilities: [], name: "Arqueiros", id: "arqueiros" },
-  { abilities: [], name: "Mosqueteiro", id: "mosqueteiro" },
-  { abilities: [], name: "Arqueiro Mágico", id: "arqueiro_magico" },
+  { abilities: [], name: "Arqueiros", id: "arqueiros", category: "arqueiros" },
+  {
+    abilities: [],
+    name: "Mosqueteiro",
+    id: "mosqueteiro",
+    category: "arqueiros",
+  },
+  {
+    abilities: [],
+    name: "Arqueiro Mágico",
+    id: "arqueiro_magico",
+    category: "arqueiros",
+  },
 
   // Guerreiros
-  { abilities: [], name: "Guerreiro", id: "guerreiro" },
-  { abilities: [], name: "Cavaleiro", id: "cavaleiro" },
-  { abilities: [], name: "Lanceiro", id: "lanceiro" },
-  { abilities: [], name: "Soldado", id: "soldado" },
-  { abilities: [], name: "Gladiador", id: "gladiador" },
-  { abilities: [], name: "Samurai", id: "samurai" },
+  { abilities: [], name: "Guerreiro", id: "guerreiro", category: "guerreiros" },
+  { abilities: [], name: "Cavaleiro", id: "cavaleiro", category: "guerreiros" },
+  { abilities: [], name: "Lanceiro", id: "lanceiro", category: "guerreiros" },
+  { abilities: [], name: "Soldado", id: "soldado", category: "guerreiros" },
+  { abilities: [], name: "Gladiador", id: "gladiador", category: "guerreiros" },
+  { abilities: [], name: "Samurai", id: "samurai", category: "guerreiros" },
 
   // Bárbaros
-  { abilities: [], name: "Bárbaro", id: "barbaro" },
-  { abilities: bersekAbilities, name: "Berserker", id: "berserker" },
-  { abilities: [], name: "Bárbaro Rúnico", id: "barbaro_runico" },
-  { abilities: [], name: "Monge", id: "monge" },
+  { abilities: [], name: "Bárbaro", id: "barbaro", category: "barbaros" },
+  {
+    abilities: bersekAbilities,
+    name: "Berserker",
+    id: "berserker",
+    category: "barbaros",
+  },
+  {
+    abilities: [],
+    name: "Bárbaro Rúnico",
+    id: "barbaro_runico",
+    category: "barbaros",
+  },
+  { abilities: [], name: "Monge", id: "monge", category: "barbaros" },
 
   // Magos
-  { abilities: [], name: "Mago", id: "mago" },
-  { abilities: [], name: "Mago Espiritual", id: "mago_espiritual" },
-  { abilities: [], name: "Mago de Guerra", id: "mago_de_guerra" },
-  { abilities: [], name: "Mago Infernal", id: "mago_infernal" },
-  { abilities: [], name: "Bardo", id: "bardo" },
+  { abilities: [], name: "Mago", id: "mago", category: "magos" },
+  {
+    abilities: [],
+    name: "Mago Espiritual",
+    id: "mago_espiritual",
+    category: "magos",
+  },
+  {
+    abilities: [],
+    name: "Mago de Guerra",
+    id: "mago_de_guerra",
+    category: "magos",
+  },
+  {
+    abilities: [],
+    name: "Mago Infernal",
+    id: "mago_infernal",
+    category: "magos",
+  },
+  { abilities: [], name: "Bardo", id: "bardo", category: "magos" },
 
   // Feiticeiro
-  { abilities: [], name: "Feiticeiro", id: "feiticeiro" },
+  {
+    abilities: [],
+    name: "Feiticeiro",
+    id: "feiticeiro",
+    category: "feiticeiros",
+  },
 
   // Druidas
-  { abilities: [], name: "Druida", id: "druida" },
-  { abilities: [], name: "Xamã", id: "xama" },
-  { abilities: [], name: "Druida Sombrio", id: "druida_sombrio" },
+  { abilities: [], name: "Druida", id: "druida", category: "druidas" },
+  { abilities: [], name: "Xamã", id: "xama", category: "druidas" },
+  {
+    abilities: [],
+    name: "Druida Sombrio",
+    id: "druida_sombrio",
+    category: "druidas",
+  },
 
   // Bruxos
-  { abilities: [], name: "Bruxo", id: "bruxo" },
-  { abilities: [], name: "Cavaleiro das Trevas", id: "cavaleiro_das_trevas" },
+  { abilities: [], name: "Bruxo", id: "bruxo", category: "bruxos" },
+  {
+    abilities: [],
+    name: "Cavaleiro das Trevas",
+    id: "cavaleiro_das_trevas",
+    category: "bruxos",
+  },
 
   // Clérigos
-  { abilities: [], name: "Clérigo", id: "clerigo" },
-  { abilities: [], name: "Curandeiro Guerreiro", id: "curandeiro_guerreiro" },
-  { abilities: [], name: "Paladino", id: "paladino" },
-  { abilities: [], name: "Guardião", id: "guardiao" },
+  { abilities: [], name: "Clérigo", id: "clerigo", category: "clerigos" },
+  {
+    abilities: [],
+    name: "Curandeiro Guerreiro",
+    id: "curandeiro_guerreiro",
+    category: "clerigos",
+  },
+  { abilities: [], name: "Paladino", id: "paladino", category: "clerigos" },
+  { abilities: [], name: "Guardião", id: "guardiao", category: "clerigos" },
 
   // Artífice
-  { abilities: [], name: "Artífice", id: "artifice" },
+  { abilities: [], name: "Artífice", id: "artifice", category: "artifice" },
 
   // Caçadores
-  { abilities: [], name: "Caçador", id: "cacador" },
-  { abilities: [], name: "Domador de Monstros", id: "domador_de_monstros" },
-  { abilities: [], name: "Cozinheiro de Feras", id: "cozinheiro_de_feras" },
-  { abilities: [], name: "Caçador Espiritual", id: "cacador_espiritual" },
+  { abilities: [], name: "Caçador", id: "cacador", category: "cacadores" },
+  {
+    abilities: [],
+    name: "Domador de Monstros",
+    id: "domador_de_monstros",
+    category: "cacadores",
+  },
+  {
+    abilities: [],
+    name: "Cozinheiro de Feras",
+    id: "cozinheiro_de_feras",
+    category: "cacadores",
+  },
+  {
+    abilities: [],
+    name: "Caçador Espiritual",
+    id: "cacador_espiritual",
+    category: "cacadores",
+  },
 
   // Ladinos
-  { abilities: [], name: "Ladino", id: "ladino" },
-  { abilities: [], name: "Pirata", id: "pirata" },
-  { abilities: ninjaAbilities, name: "Ninja", id: "ninja" },
+  { abilities: [], name: "Ladino", id: "ladino", category: "ladinos" },
+  { abilities: [], name: "Pirata", id: "pirata", category: "ladinos" },
+  {
+    abilities: ninjaAbilities,
+    name: "Ninja",
+    id: "ninja",
+    category: "ladinos",
+  },
 ]
